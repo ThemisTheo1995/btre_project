d8c2820336b27cfd2a2569230a83389b75c9efb0
Theo <themistheodoratos@gmail.com>
2020-06-01 16:10:35 +0300

    Local settings file x2

--- btre/settings.py
+++ btre/settings.py
@@ -142,4 +142,8 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-#email config
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
