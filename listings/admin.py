9f69ff62d3e6de4b2ec9e1067d41a0279f3d2242
Theo <themistheodoratos@gmail.com>
2020-06-11 18:09:32 +0300

    Footer and Top Bar UI changes

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Listing
 
+
+
 class ListingAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
   list_display_links = ('id', 'title')
@@ -10,3 +12,7 @@ class ListingAdmin(admin.ModelAdmin):
   list_per_page = 25
 
 admin.site.register(Listing, ListingAdmin)
+
+
+
+
